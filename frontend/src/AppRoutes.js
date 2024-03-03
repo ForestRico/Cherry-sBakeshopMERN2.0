254b74248628c8ca76b641961f9bc60eb50bd859
ForestRico <126733661+ForestRico@users.noreply.github.com>
2024-03-03 16:50:33 +1100

    Created Cart page component, css and approute

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home/HomePage';
 import FoodPage from './pages/Home/Food/FoodPage';
+import CartPage from './pages/Home/Cart/CartPage';
 
 export default function AppRoutes() {
   return (
@@ -8,7 +9,8 @@ export default function AppRoutes() {
       <Route path="/" element={<HomePage/>} />
       <Route path="/search/:searchTerm" element={<HomePage/>} />  {/*'search' is a constant term seen in the address bar. 'searchTerm' a wrote parameter that we can extract from the homePage and use to search*/}
       <Route path="/tag/:tag" element={<HomePage/>} /> 
-      <Route path="/food/:id" element={<FoodPage/>} />    
+      <Route path="/food/:id" element={<FoodPage/>} />
+      <Route path="/cart" element={<CartPage/>} />    
     </Routes>
   )
 }
