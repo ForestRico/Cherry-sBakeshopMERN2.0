5029d9eb7cd9eedaa332a2236b585b06e73c9542
ForestRico <126733661+ForestRico@users.noreply.github.com>
2024-03-04 15:03:11 +1100

    basic backend login functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router.js';
+import userRouter from './routers/user,router.js';
 
 const app = express();
+// Since we're using 'req.body' and want to send our data to the server as JSON, we need to tell express to use JSON as the body
+// This code parses JSON body
+app.use(express.json());
 
 app.use(cors({
     credentials:true,
@@ -13,6 +17,7 @@ app.use(cors({
 // Front end is already running on port3000, backend must run on a separate port (5000)
 
 app.use('/api/foods', foodRouter);
+app.use('/api/users', userRouter);
 
 
 const PORT = 5000;
