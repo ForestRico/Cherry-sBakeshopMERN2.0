afde96fc79e49592b0f3e56c2a1d7ed6766c8dd6
ForestRico <126733661+ForestRico@users.noreply.github.com>
2024-03-07 22:42:26 +1100

    Fixed MongoDB Connections working well on backend

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URI);
+        // await mongoose.connect('mongodb://localhost:5000/BakeshopDB');
         console.log("Database connected successfully");
     } catch (err) {
         console.error(`Database connection failed: ${err.message}`);
